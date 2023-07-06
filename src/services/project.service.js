e9c20bf9fe538b9c1d3a969433fdb7acd4122903
Tiago Rato <tiagorato13@hotmail.com>
2023-07-06 12:24:55 +0100

    mc

--- src/services/project.service.js
+++ src/services/project.service.js
@@ -3,7 +3,7 @@ import axios from "axios";
 class ProjectService {
   constructor() {
     this.api = axios.create({
-      baseURL: import.meta.env.VITE_API_URL,
+      baseURL: import.meta.env.VITE_API_URL || "https://tiagorato.cyclic.app",
       /* withCredentials: true  */
     });
 
